77c7666e9ed422b92839bffb0761c6b4f3cfabdf
Rezaei-ali <ali1998rezaei3@yahoo.com>
2024-02-06 21:49:40 +0330

    modify database.

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgres://uwftsive:sMm4_yt96e62HpIVlryZD5ol2_SLVvt_@tiny.db.elephantsql.com/uwftsive'
+SQLALCHEMY_DATABASE_URL = 'postgresql://uwftsive:sMm4_yt96e62HpIVlryZD5ol2_SLVvt_@tiny.db.elephantsql.com/uwftsive'
 
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
