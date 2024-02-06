c5b7836ab77c536f135c1808d8c77b9edda9a0c4
Rezaei-ali <ali1998rezaei3@yahoo.com>
2024-02-06 21:48:21 +0330

    change connection.

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+
 SQLALCHEMY_DATABASE_URL = 'postgresql://uwftsive:sMm4_yt96e62HpIVlryZD5ol2_SLVvt_@tiny.db.elephantsql.com/uwftsive'
 
 # MYSQL Series
